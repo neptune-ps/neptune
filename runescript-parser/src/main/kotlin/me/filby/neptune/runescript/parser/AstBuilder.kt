1f07abd917ba9efc3bd573b4d17528ca875efa74
Joshua Filby <joshua@filby.me>
2021-04-28 03:58:17 -0500

    refactor: cleanup AstBuilder by importing inner classes

--- runescript-parser/src/main/kotlin/me/filby/neptune/runescript/parser/AstBuilder.kt
+++ runescript-parser/src/main/kotlin/me/filby/neptune/runescript/parser/AstBuilder.kt
@@ -1,8 +1,18 @@
 package me.filby.neptune.runescript.parser
 
-import me.filby.neptune.runescript.antlr.RuneScriptParser
+import me.filby.neptune.runescript.antlr.RuneScriptParser.BinaryExpressionContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.BooleanLiteralContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.CalcExpressionContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.CallExpressionContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.CharacterLiteralContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.ExpressionListContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.ExpressionStatementContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.IdentifierContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.IntegerLiteralContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.NullLiteralContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.ScriptContext
+import me.filby.neptune.runescript.antlr.RuneScriptParser.ScriptFileContext
 import me.filby.neptune.runescript.antlr.RuneScriptParserBaseVisitor
-import me.filby.neptune.runescript.ast.expr.Identifier
 import me.filby.neptune.runescript.ast.Node
 import me.filby.neptune.runescript.ast.Script
 import me.filby.neptune.runescript.ast.ScriptFile
@@ -12,6 +22,7 @@ import me.filby.neptune.runescript.ast.expr.CalcExpression
 import me.filby.neptune.runescript.ast.expr.CallExpression
 import me.filby.neptune.runescript.ast.expr.CharacterLiteral
 import me.filby.neptune.runescript.ast.expr.Expression
+import me.filby.neptune.runescript.ast.expr.Identifier
 import me.filby.neptune.runescript.ast.expr.IntegerLiteral
 import me.filby.neptune.runescript.ast.expr.NullLiteral
 import me.filby.neptune.runescript.ast.statement.ExpressionStatement
@@ -19,11 +30,11 @@ import me.filby.neptune.runescript.ast.statement.Statement
 
 public class AstBuilder : RuneScriptParserBaseVisitor<Node>() {
 
-    override fun visitScriptFile(ctx: RuneScriptParser.ScriptFileContext): ScriptFile {
+    override fun visitScriptFile(ctx: ScriptFileContext): ScriptFile {
         return ScriptFile(ctx.script().map { visit(it) as Script })
     }
 
-    override fun visitScript(ctx: RuneScriptParser.ScriptContext): Script {
+    override fun visitScript(ctx: ScriptContext): Script {
         return Script(
             trigger = visit(ctx.trigger) as Identifier,
             name = visit(ctx.name) as Identifier,
@@ -31,11 +42,11 @@ public class AstBuilder : RuneScriptParserBaseVisitor<Node>() {
         )
     }
 
-    override fun visitExpressionStatement(ctx: RuneScriptParser.ExpressionStatementContext): ExpressionStatement {
+    override fun visitExpressionStatement(ctx: ExpressionStatementContext): ExpressionStatement {
         return ExpressionStatement(visit(ctx.expression()) as Expression)
     }
 
-    override fun visitBinaryExpression(ctx: RuneScriptParser.BinaryExpressionContext): Node {
+    override fun visitBinaryExpression(ctx: BinaryExpressionContext): Node {
         return BinaryExpression(
             left = visit(ctx.expression(0)) as Expression,
             operator = ctx.op.text,
@@ -43,18 +54,18 @@ public class AstBuilder : RuneScriptParserBaseVisitor<Node>() {
         )
     }
 
-    override fun visitCalcExpression(ctx: RuneScriptParser.CalcExpressionContext): Node {
+    override fun visitCalcExpression(ctx: CalcExpressionContext): Node {
         return CalcExpression(visit(ctx.expression()) as Expression)
     }
 
-    override fun visitCallExpression(ctx: RuneScriptParser.CallExpressionContext): Node {
+    override fun visitCallExpression(ctx: CallExpressionContext): Node {
         return CallExpression(
             name = visit(ctx.identifier()) as Identifier,
             arguments = ctx.expressionList().visit()
         )
     }
 
-    override fun visitIntegerLiteral(ctx: RuneScriptParser.IntegerLiteralContext): IntegerLiteral {
+    override fun visitIntegerLiteral(ctx: IntegerLiteralContext): IntegerLiteral {
         val text = ctx.text
         if (text.length > 1 && text[0] == '0' && (text[1] == 'x' || text[1] == 'X')) {
             // hex, trim 0x
@@ -63,29 +74,29 @@ public class AstBuilder : RuneScriptParserBaseVisitor<Node>() {
         return IntegerLiteral(text.toInt())
     }
 
-    override fun visitBooleanLiteral(ctx: RuneScriptParser.BooleanLiteralContext): BooleanLiteral {
+    override fun visitBooleanLiteral(ctx: BooleanLiteralContext): BooleanLiteral {
         return BooleanLiteral(ctx.text.toBoolean())
     }
 
-    override fun visitCharacterLiteral(ctx: RuneScriptParser.CharacterLiteralContext): Node {
+    override fun visitCharacterLiteral(ctx: CharacterLiteralContext): Node {
         // TODO support for escaping
         return CharacterLiteral(ctx.text[1])
     }
 
-    override fun visitNullLiteral(ctx: RuneScriptParser.NullLiteralContext?): NullLiteral {
+    override fun visitNullLiteral(ctx: NullLiteralContext?): NullLiteral {
         return NullLiteral
     }
 
-    override fun visitIdentifier(ctx: RuneScriptParser.IdentifierContext): Identifier {
+    override fun visitIdentifier(ctx: IdentifierContext): Identifier {
         return Identifier(ctx.text)
     }
 
     /**
-     * Helper that converts an [RuneScriptParser.ExpressionListContext] to a [List] of [Expression]s.
+     * Helper that converts an [ExpressionListContext] to a [List] of [Expression]s.
      *
      * @return A list of expression if defined. If there are no expressions the list will be empty.
      */
-    private fun RuneScriptParser.ExpressionListContext?.visit(): List<Expression> {
+    private fun ExpressionListContext?.visit(): List<Expression> {
         val expressions = this?.expression() ?: return emptyList()
         return expressions.map { visit(it) as Expression }
     }
