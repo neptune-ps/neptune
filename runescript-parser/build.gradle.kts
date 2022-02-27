d4a099bc326929a3c4376f3283d8c3b651c3e035
Joshua Filby <joshua@filby.me>
2022-02-27 04:59:02 -0600

    chore: fix warnings from gradle

--- runescript-parser/build.gradle.kts
+++ runescript-parser/build.gradle.kts
@@ -37,5 +37,10 @@ tasks.generateGrammarSource {
     arguments = listOf("-no-listener", "-visitor", "-package", antlrPackage) + grammars
 }
 
+tasks.withType<org.jmailen.gradle.kotlinter.tasks.LintTask> {
+    // explicit dependency
+    dependsOn(tasks.generateGrammarSource)
+}
+
 // disable ktlint on tests for now, currently not happy with tests in general
 tasks.getByPath(":runescript-parser:lintKotlinTest").enabled = false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,6 @@ pluginManagement {
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("VERSION_CATALOGS")
 
 include(
     "runescript-compiler",
