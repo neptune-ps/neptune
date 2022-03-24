69148d9540232c3b97fbbdea50e99d5817a27633
Joshua Filby <joshua@filby.me>
2022-03-24 01:39:53 -0500

    chore: fix typos in docs

--- runescript-compiler/src/main/kotlin/me/filby/neptune/runescript/compiler/type/wrapped/WrappedType.kt
+++ runescript-compiler/src/main/kotlin/me/filby/neptune/runescript/compiler/type/wrapped/WrappedType.kt
@@ -3,10 +3,10 @@ package me.filby.neptune.runescript.compiler.type.wrapped
 import me.filby.neptune.runescript.compiler.type.Type
 
 /**
- * A type that container an inner type. This is intended for more complex types
+ * A type that contains an inner type. This is intended for more complex types
  * that resolve to a different type depending on how they're accessed. This is
  * necessary for some cases where you want to verify a reference is of a type
- * without seeing that the "execution" type would be.
+ * without seeing what the "execution" type would be.
  *
  * @see ArrayType
  */
