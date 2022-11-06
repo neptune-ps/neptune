efd65f49dcfc7fc4be4eb6e58886f1e941cae4bc
Joshua Filby <joshua@filby.me>
2022-11-06 06:07:51 -0600

    refactor: move BaseScriptWriter helpers to companion object

--- runescript-compiler/src/main/kotlin/me/filby/neptune/runescript/compiler/writer/BaseScriptWriter.kt
+++ runescript-compiler/src/main/kotlin/me/filby/neptune/runescript/compiler/writer/BaseScriptWriter.kt
@@ -10,29 +10,78 @@ import me.filby.neptune.runescript.compiler.type.wrapped.ArrayType
  * a script.
  */
 public abstract class BaseScriptWriter : ScriptWriter {
-    // RuneScript.LocalTable helpers
+    public companion object {
+        // RuneScript helpers
 
-    /**
-     * Returns the total number of parameters with a base var type of [baseType].
-     */
-    public fun RuneScript.LocalTable.getParameterCount(baseType: BaseVarType): Int {
-        return parameters.count { it.type.baseType == baseType }
-    }
+        /**
+         * Returns a mapping of instruction index to line number. This modifies the
+         * list of instruction by removing any instruction with an opcode of
+         * [Opcode.LINENUMBER].
+         */
+        public fun RuneScript.generateLineNumberTable(): TreeMap<Int, Int> {
+            val table = TreeMap<Int, Int>()
+            var index = 0
+            for (block in blocks) {
+                val it = block.instructions.iterator()
+                while (it.hasNext()) {
+                    val instruction = it.next()
 
-    /**
-     * Returns the total number of local variables with a base var type of [baseType].
-     */
-    public fun RuneScript.LocalTable.getLocalCount(baseType: BaseVarType): Int {
-        return all.count { it.type.baseType == baseType }
-    }
+                    // check if the instruction is a linenumber
+                    if (instruction.opcode == Opcode.LINENUMBER) {
+                        // add it to the table
+                        table[index] = instruction.operand as Int
 
-    /**
-     * Finds the unique identifier for the given [local] variable.
-     */
-    public fun RuneScript.LocalTable.getVariableId(local: LocalVariableSymbol): Int {
-        val isArray = local.type is ArrayType
-        return all.asSequence()
-            .filter { isArray && it.type is ArrayType || !isArray && it.type.baseType == local.type.baseType }
-            .indexOf(local)
+                        // remove it from instructions
+                        it.remove()
+                        continue
+                    }
+
+                    // increment the instruction index
+                    index++
+                }
+            }
+            return table
+        }
+
+        /**
+         * Returns a mapping of where all [Label]s are located.
+         *
+         * **WARNING**: This should be called **after** all other things that modify
+         * the instruction lists.
+         */
+        public fun RuneScript.generateJumpTable(): Map<Label, Int> {
+            val table = mutableMapOf<Label, Int>()
+            var index = 0
+            for (block in blocks) {
+                table[block.label] = index
+                index += block.instructions.size
+            }
+            return table
+        }
+
+        // RuneScript.LocalTable helpers
+
+        /**
+         * Returns the total number of parameters with a base var type of [baseType].
+         */
+        public fun RuneScript.LocalTable.getParameterCount(baseType: BaseVarType): Int {
+            return parameters.count { it.type.baseType == baseType }
+        }
+
+        /**
+         * Returns the total number of local variables with a base var type of [baseType].
+         */
+        public fun RuneScript.LocalTable.getLocalCount(baseType: BaseVarType): Int {
+            return all.count { it.type.baseType == baseType }
+        }
+
+        /**
+         * Finds the unique identifier for the given [local] variable.
+         */
+        public fun RuneScript.LocalTable.getVariableId(local: LocalVariableSymbol): Int {
+            val isArray = local.type is ArrayType
+            return all.asSequence()
+                .filter { isArray && it.type is ArrayType || !isArray && it.type.baseType == local.type.baseType }
+                .indexOf(local)
+        }
     }
-}
