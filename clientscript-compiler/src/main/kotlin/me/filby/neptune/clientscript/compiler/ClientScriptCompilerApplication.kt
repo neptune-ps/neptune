7951c6669d5295222160ec7a1fddcee2f9f58d60
Joshua Filby <joshua@filby.me>
2023-03-28 16:32:23 -0500

    feat: use BinaryFileScriptWriter when running compiler

--- clientscript-compiler/src/main/kotlin/me/filby/neptune/clientscript/compiler/ClientScriptCompilerApplication.kt
+++ clientscript-compiler/src/main/kotlin/me/filby/neptune/clientscript/compiler/ClientScriptCompilerApplication.kt
@@ -1,19 +1,23 @@
 package me.filby.neptune.clientscript.compiler
 
-import me.filby.neptune.runescript.compiler.codegen.script.RuneScript
-import me.filby.neptune.runescript.compiler.writer.ScriptWriter
+import me.filby.neptune.clientscript.compiler.writer.BinaryFileScriptWriter
 import kotlin.io.path.Path
 import kotlin.io.path.exists
 import kotlin.io.path.readLines
 
 fun main(args: Array<String>) {
-    if (args.size != 1) {
-        error("usage: compiler.jar [src path]")
+    if (args.size != 2) {
+        error("usage: compiler.jar [src path] [output path]")
     }
 
     val srcPath = Path(args[0])
     if (!srcPath.exists()) {
         error("$srcPath does not exist.")
+    }
+
+    val outputPath = Path(args[1])
+    if (!outputPath.exists()) {
+        error("$outputPath does not exist.")
     }
 
     val mapper = SymbolMapper()
@@ -29,14 +33,8 @@ fun main(args: Array<String>) {
         }
     }
 
-    val compiler = ClientScriptCompiler(
-        srcPath,
-        object : ScriptWriter {
-            override fun write(script: RuneScript) {
-            }
-        },
-        mapper
-    )
+    val writer = BinaryFileScriptWriter(outputPath, mapper)
+    val compiler = ClientScriptCompiler(srcPath, writer, mapper)
     compiler.setup()
     compiler.run()
 }
