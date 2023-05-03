f5e5308177c8616c6355b6673e093cc197f49f63
Joshua Filby <joshua@filby.me>
2023-05-03 17:14:13 -0500

    feat: add application task to clientscript-compiler

--- clientscript-compiler/build.gradle.kts
+++ clientscript-compiler/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
+    application
     kotlin("jvm")
 }
 
 dependencies {
-    api(project(":runescript-compiler"))
+    api(project(":runescript-compiler")) {
+        exclude("com.ibm.icu", "icu4j")
+    }
     implementation(libs.netty.buffer)
     runtimeOnly(libs.logback)
+}
+
+application {
+    applicationName = "cs2"
+    mainClass.set("me.filby.neptune.clientscript.compiler.ClientScriptCompilerApplicationKt")
 }
