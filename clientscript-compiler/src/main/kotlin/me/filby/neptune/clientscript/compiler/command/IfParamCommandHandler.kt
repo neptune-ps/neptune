60cb97c4d65d7687983495e226953fe4eec1aeda
Joshua Filby <joshua@filby.me>
2025-06-29 05:19:52 -0500

    feat: add {if,cc}_{param,setparam} commands

--- clientscript-compiler/src/main/kotlin/me/filby/neptune/clientscript/compiler/command/IfParamCommandHandler.kt
+++ clientscript-compiler/src/main/kotlin/me/filby/neptune/clientscript/compiler/command/IfParamCommandHandler.kt
@@ -1,27 +1,40 @@
 package me.filby.neptune.clientscript.compiler.command
 
 import me.filby.neptune.clientscript.compiler.type.ParamType
+import me.filby.neptune.clientscript.compiler.type.ScriptVarType
 import me.filby.neptune.runescript.compiler.configuration.command.DynamicCommandHandler
 import me.filby.neptune.runescript.compiler.configuration.command.TypeCheckingContext
 import me.filby.neptune.runescript.compiler.type
 import me.filby.neptune.runescript.compiler.type.MetaType
+import me.filby.neptune.runescript.compiler.type.PrimitiveType
 import me.filby.neptune.runescript.compiler.type.TupleType
-import me.filby.neptune.runescript.compiler.type.Type
 
-class ParamCommandHandler(private val type: Type) : DynamicCommandHandler {
+/**
+ * An implementation of [DynamicCommandHandler] that adds support for type checking
+ * the `if_param` and `cc_param` commands within scripts.
+ *
+ * Example:
+ * ```
+ * def_int $i = if_param(some_int_param, $comp, null);
+ * def_int $j = cc_param(some_int_param);
+ * ```
+ */
+class IfParamCommandHandler(private val cc: Boolean) : DynamicCommandHandler {
     override fun TypeCheckingContext.typeCheck() {
-        // check first argument is the defined type
-        checkArgument(0, type)
-
-        // check the second argument is a param reference
-        val paramExpr = checkArgument(1, PARAM_ANY)
+        // check param reference
+        val paramExpr = checkArgument(0, ParamCommandHandler.PARAM_ANY)
         val paramReturnType = (paramExpr?.type as? ParamType)?.inner
+        if (!cc) {
+            checkArgument(1, ScriptVarType.COMPONENT)
+            checkArgument(2, PrimitiveType.INT)
+        }
 
         // define the expected types based on what is currently known
-        val expectedTypes = TupleType(
-            type,
-            ParamType(paramReturnType ?: MetaType.Any),
-        )
+        val expectedTypes = if (!cc) {
+            IF_BASE_EXPECTED_TYPES
+        } else {
+            CC_BASE_EXPECTED_TYPES
+        }
 
         // compare the expected types with the actual types
         if (!checkArgumentTypes(expectedTypes)) {
@@ -39,7 +52,12 @@ class ParamCommandHandler(private val type: Type) : DynamicCommandHandler {
         expression.type = paramReturnType
     }
 
-    companion object {
-        val PARAM_ANY = ParamType(MetaType.Any)
+    private companion object {
+        val IF_BASE_EXPECTED_TYPES = TupleType(
+            ParamCommandHandler.PARAM_ANY,
+            ScriptVarType.COMPONENT,
+            PrimitiveType.INT,
+        )
+        val CC_BASE_EXPECTED_TYPES = TupleType(ParamCommandHandler.PARAM_ANY)
     }
 }
